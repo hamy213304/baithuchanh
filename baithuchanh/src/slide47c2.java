64441d249627136ccf5c882e5c0126d4470846b9
hamy213304 <nmy22112004@gmail.com>
2024-01-30 21:29:36 +0700

    De bai trong slide

---
+++ baithuchanh/src/slide47c2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class slide47c2 {
+    public static void main(String[] args) {
+        int i, tong =0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap lan luot cac so nguyen: ");
+        do {
+            System.out.println("So nguyen: ");
+          i = sc.nextInt();
+          tong +=i; 
+        } while (tong<100);
+         System.out.print("Tong cac so nguyen vua nhap = " + tong);
+         sc.close();
+        }
+}
---
+++ baithuchanh/src/slide55c2.java
@@ -0,0 +1,11 @@
+public class slide55c2 {
+    public static void main(String[] args) {
+    int i;
+        System.out.println("Cac so chia het cho 5 la: ");
+        for (i=0;i<=20;i++)
+        {
+            if (i%5 !=0) {continue;}
+            System.out.println(i);
+        }
+    }
+}
