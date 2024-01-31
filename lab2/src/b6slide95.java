5f4813d940ae091b3840232c1e3df85b8e7a19ba
hamy213304 <nmy22112004@gmail.com>
2024-01-31 11:26:15 +0700

    slide cuoi chuong 2

---
+++ lab2/src/b6slide95.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class b6slide95 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n;
+        long gthua = 1;
+        System.out.println("Nhap vao mot so nguyen duong bat ky: ");
+        n = sc.nextInt();
+        for (int i=1;i<=n;i++){
+        gthua = gthua*i;}
+        System.out.println("Giai thua cua so " +n+" = "+gthua);
+        sc.close();
+    }
+}
