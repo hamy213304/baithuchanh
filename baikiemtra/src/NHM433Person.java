4489c0b290095576c5aec949788deef7bd31f46a
hamy213304 <nmy22112004@gmail.com>
2024-05-22 10:07:27 +0700

    NHM433_Cau2_update

--- baikiemtra/src/NHM433Person.java
+++ baikiemtra/src/NHM433Person.java
@@ -6,20 +6,45 @@ public class NHM433Person {
     public int masv;
     public double diem;
     Scanner sc = new Scanner(System.in);
-    public void nhapTT[]{
-        System.out.println("Yeu cau: nhap cac thong tin sau");
-        System.out.println("Nhap ho va ten: ");
-        hoten = sc.nextLine();
-        System.out.println("Nhap ma sinh vien: ");
-        masv = sc.nextInt();
-        System.out.println("Nhap diem: ");
-        diem = sc.nextDouble();
+    public void nhapTT(){
+      
     }
-    public void xuatTT[]{
-        System.out.println("Ho va ten: "+hoten);
-        System.out.println("Ma sinh vien: "+masv);
-        System.out.println("Diem: "+diem);
+    public void xuatTT(){
+       
+    }
+    public static void main(String[] args) {
+    LinkedList<String> ll = new LinkedList<>();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Nhap so luong nguoi: ");
+    int n = sc.nextInt();
+    public void themTT(){
+        System.out.println("Nhap thong tin can them: ");
+        String them=sc.nextLine();
+        ll.add(them);
+    }
+    public void suaTT(){
+        System.out.println("Ban muon sua phan nao? Ho va ten=1,Ma sinh vien=2,Diem=3");
+        int sua=sc.nextInt();
+        if(sua==1){
+            System.out.println("Nhap lai ho va ten: ");
+            hoten = sc.nextLine();
+        } else if(sua==2){
+            System.out.println("Nhap lai ma sinh vien");
+            masv=sc.nextInt();
+        } else if(sua ==3){
+            System.out.println("Nhap lai diem: ");
+            diem = sc.nextInt();
+        }
+    }
+    public void xoaTT(){
+        System.out.println("Nhap thong tin muon xoa: ");
+        String xoa = sc.nextLine();
+        ll.remove(xoa);
+    }
+    public void hienthiTT(){
+        System.out.println("So luong nguoi trong list: "+n);
     }
     
     }
     
+}
---
+++ baikiemtra/src/NHM433Student.java
@@ -0,0 +1,18 @@
+public class NHM433Student extends NHM433Person {
+    @Override
+    public void nhapTT(){
+        System.out.println("Yeu cau: nhap cac thong tin sau");
+        System.out.println("Nhap ho va ten: ");
+        hoten = sc.nextLine();
+        System.out.println("Nhap ma sinh vien: ");
+        masv = sc.nextInt();
+        System.out.println("Nhap diem: ");
+        diem = sc.nextDouble();
+    }
+    public void xuatTT(){
+        System.out.println("Ho va ten: "+hoten);
+        System.out.println("Ma sinh vien: "+masv);
+        System.out.println("Diem: "+diem);
+    }
+
+}
