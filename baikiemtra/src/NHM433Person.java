4f5d8bb8490cb373d63e0791d341d57742f4891f
hamy213304 <nmy22112004@gmail.com>
2024-05-22 09:46:58 +0700

    NHM433_Cau2

--- baikiemtra/src/NHM433Person.java
+++ baikiemtra/src/NHM433Person.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class NHM433Person {
     public static void main(String[] args) {
     String hoten;
     int masv;
     double diem;
+    Scanner sc = new Scanner(System.in);
+    public void nhapTT[]{
+        System.out.println("Yeu cau: nhap cac thong tin sau");
+        System.out.println("Nhap ho va ten: ");
+        hoten = sc.nextLine();
+        System.out.println("Nhap ma sinh vien: ");
+        masv = sc.nextInt();
+        System.out.println("Nhap diem: ");
+        diem = sc.nextDouble();
+    }
+    public void xuatTT[]{
+        System.out.println("Ho va ten: "+hoten);
+        System.out.println("Ma sinh vien: "+masv);
+        System.out.println("Diem: "+diem);
+    }
     
     }
     
